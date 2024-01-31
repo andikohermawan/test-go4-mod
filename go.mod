42211458c8fbdf20a4fb6f48195cd30134b368c1
Andiko Hermawan <andikohermawan44@gmail.com>
2024-01-31 10:07:56 +0700

    Module-sayHello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andikohermawan/test-go4-mod
+
+go 1.21.4
---
+++ test-say-Hello.go
@@ -0,0 +1,6 @@
+package main
+
+///test for module in git
+func sayHello() string {
+	return "good morning my neighbor!!!"
+}
